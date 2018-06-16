8ba412eb9338d4911314cd7509a7f0840a752652
FHellmann <fhellmann93@googlemail.com>
2018-06-17 00:57:38 +0200

    Fix #50 and fix #45

--- app/src/main/java/com/edu/uni/augsburg/uniatron/MainApplication.java
+++ app/src/main/java/com/edu/uni/augsburg/uniatron/MainApplication.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 
 import com.edu.uni.augsburg.uniatron.domain.AppDatabase;
 import com.edu.uni.augsburg.uniatron.domain.DataRepository;
+import com.edu.uni.augsburg.uniatron.notification.NotificationChannels;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 
@@ -20,17 +21,14 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // initialize logger
         Logger.addLogAdapter(new AndroidLogAdapter() {
             @Override
             public boolean isLoggable(final int priority, @Nullable final String tag) {
                 return BuildConfig.DEBUG;
             }
         });
-
-        // initialize app database
         mDataRepository = new DataRepository(AppDatabase.create(this));
-
+        NotificationChannels.setupChannels(this);
     }
 
     /**
--- app/src/main/java/com/edu/uni/augsburg/uniatron/service/handler/PackageAddedHandler.java
+++ app/src/main/java/com/edu/uni/augsburg/uniatron/service/handler/PackageAddedHandler.java
@@ -5,12 +5,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationManagerCompat;
 
 import com.edu.uni.augsburg.uniatron.SharedPreferencesHandler;
-import com.edu.uni.augsburg.uniatron.notification.NotificationChannels;
+import com.edu.uni.augsburg.uniatron.notification.AppNotificationBuilder;
 import com.edu.uni.augsburg.uniatron.notification.builder.PackageAddedNotificationBuilder;
+import com.edu.uni.augsburg.uniatron.service.AddBlacklistEntryService;
 import com.orhanobut.logger.Logger;
 
 /**
@@ -21,19 +23,28 @@ import com.orhanobut.logger.Logger;
 public class PackageAddedHandler extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
+        Logger.d("Package action received: " + intent);
         if (Intent.ACTION_PACKAGE_ADDED.equals(intent.getAction())) {
-            postNotification(context, intent);
+            if (intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)) {
+                Logger.d("This was an update from the play store ("
+                        + getPackageName(intent) + ")");
+                context.startService(new Intent(context, AddBlacklistEntryService.class)
+                        .putExtra(Intent.EXTRA_RETURN_RESULT, getPackageName(intent)));
+            } else {
+                Logger.d("A new package was found (" + getPackageName(intent) + ")");
+                postNotification(context, intent);
+            }
         } else if (Intent.ACTION_PACKAGE_REMOVED.equals(intent.getAction())) {
             removePackageFromBlacklist(context, intent);
         }
     }
 
     private void postNotification(final Context context, final Intent intent) {
-        Logger.d("A new package was found -> notifying user");
-        NotificationChannels.setupChannels(context);
-
-        final PackageAddedNotificationBuilder builder =
-                new PackageAddedNotificationBuilder(context, intent);
+        final AppNotificationBuilder builder = new PackageAddedNotificationBuilder(
+                context,
+                getLastInstalledAppLabel(context, intent),
+                getPackageName(intent)
+        );
 
         final Notification notification = builder.build();
         final int notificationId = builder.getId();
@@ -42,10 +53,33 @@ public class PackageAddedHandler extends BroadcastReceiver {
     }
 
     private void removePackageFromBlacklist(final Context context, final Intent intent) {
+        final SharedPreferencesHandler handler = new SharedPreferencesHandler(context);
+        handler.removeAppFromBlacklist(getPackageName(intent));
+    }
+
+    private String getLastInstalledAppLabel(final Context context, final Intent intent) {
+        final PackageManager packageManager = context.getPackageManager();
+        final String addedPackageName = getPackageName(intent);
+        try {
+            return packageManager.getApplicationLabel(
+                    packageManager.getApplicationInfo(
+                            addedPackageName,
+                            0
+                    )).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new IllegalStateException("Unable to find the added package '"
+                    + addedPackageName + "'", e);
+        }
+    }
+
+    private String getPackageName(final Intent intent) {
         final Uri data = intent.getData();
-        if (data != null) {
-            final SharedPreferencesHandler handler = new SharedPreferencesHandler(context);
-            handler.removeAppFromBlacklist(data.getEncodedSchemeSpecificPart());
+        if (data == null) {
+            // If the intent does not provide the installed package name
+            throw new IllegalStateException("The intent need's to contain the added package name");
+        } else {
+            // The intent provides the installed package name
+            return data.getEncodedSchemeSpecificPart();
         }
     }
 
