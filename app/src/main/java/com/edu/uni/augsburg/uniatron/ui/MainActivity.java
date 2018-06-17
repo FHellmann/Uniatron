131d80821f9e076bf6d19e0222f6eb7fba30ab8c
FHellmann <fhellmann93@googlemail.com>
2018-06-17 21:31:12 +0200

    Remove unused imports

--- app/src/main/java/com/edu/uni/augsburg/uniatron/ui/MainActivity.java
+++ app/src/main/java/com/edu/uni/augsburg/uniatron/ui/MainActivity.java
@@ -14,8 +14,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
 
 import com.edu.uni.augsburg.uniatron.R;
 import com.edu.uni.augsburg.uniatron.service.AppTrackingService;
