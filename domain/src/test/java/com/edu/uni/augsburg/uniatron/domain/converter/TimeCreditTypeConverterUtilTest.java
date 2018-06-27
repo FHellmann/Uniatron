7c33c0e3b94d9a04659b983d5dc511654fe5c782
FHellmann <fhellmann93@googlemail.com>
2018-06-27 16:30:51 +0200

    Add tests

---
+++ domain/src/test/java/com/edu/uni/augsburg/uniatron/domain/converter/TimeCreditTypeConverterUtilTest.java
@@ -0,0 +1,31 @@
+package com.edu.uni.augsburg.uniatron.domain.converter;
+
+import com.edu.uni.augsburg.uniatron.model.TimeCreditType;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class TimeCreditTypeConverterUtilTest {
+
+    @Test
+    public void fromRawValue() {
+        final TimeCreditType learningAid = TimeCreditType.LEARNING_AID;
+        final String rawValue = learningAid.name();
+
+        final TimeCreditType timeCreditType = TimeCreditTypeConverterUtil.fromRawValue(rawValue);
+
+        assertThat(timeCreditType, is(learningAid));
+    }
+
+    @Test
+    public void fromRealValue() {
+        final TimeCreditType learningAid = TimeCreditType.LEARNING_AID;
+
+        final String timeCreditType = TimeCreditTypeConverterUtil.fromRealValue(learningAid);
+
+        assertThat(TimeCreditType.valueOf(timeCreditType), is(learningAid));
+    }
+}
