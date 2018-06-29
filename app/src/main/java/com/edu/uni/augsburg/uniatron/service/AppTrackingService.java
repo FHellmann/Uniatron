ad38cfbefa6c162bf4bcbdf3843c55c0b7cbc9df
leonpoint <l.woehrl@gmail.com>
2018-06-29 23:05:51 +0200

    fix checkstyle issues

--- app/src/main/java/com/edu/uni/augsburg/uniatron/service/AppTrackingService.java
+++ app/src/main/java/com/edu/uni/augsburg/uniatron/service/AppTrackingService.java
@@ -36,13 +36,23 @@ public class AppTrackingService extends LifecycleService {
 
     private static final int DELAY_IN_MILLISECONDS = 1000;
     private static final List<String> FILTERS = new ArrayList<>();
+    private static final int NOTIFICATION_TIME_OFFSET = 1;
+    private static final int ONE_MINUTE = 60;
+    private static final int FIVE_MINUTES = 300;
+    private static final int TEN_MINUTES = 600;
     private final AppChecker mAppChecker = new AppChecker();
     private SharedPreferencesHandler mSharedPreferencesHandler;
     private DataRepository mRepository;
     private Boolean commitStatus = false;
     private int remainingUsageTime = 10;
+    private final Observer<Integer> usageTimeObserver = new Observer<Integer>() {
+        @Override
+        public void onChanged(@Nullable final Integer integer) {
+            //Log.d(getClass().toString(), "onchanged");
+            remainingUsageTime = integer;
+        }
+    };
     private Long timePassedLearningAid;
-
     private final BroadcastReceiver mScreenEventReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(final Context context, final Intent intent) {
@@ -58,14 +68,6 @@ public class AppTrackingService extends LifecycleService {
             }
         }
     };
-    private final Observer<Integer> usageTimeObserver = new Observer<Integer>() {
-        @Override
-        public void onChanged(@Nullable final Integer integer) {
-            //Log.d(getClass().toString(), "onchanged");
-            remainingUsageTime = integer;
-        }
-    };
-
     private final Observer<Long> learningAidObserver = new Observer<Long>() {
         @Override
         public void onChanged(@Nullable final Long learningAidDiff) {
@@ -119,10 +121,12 @@ public class AppTrackingService extends LifecycleService {
 
 
     /**
-     * @param intent
-     * @param flags
-     * @param startId
-     * @return
+     * Executes the start of the service.
+     *
+     * @param intent  the intent
+     * @param flags   the flags
+     * @param startId the startid
+     * @return the way the service is started
      */
     public int onStartCommand(final Intent intent, final int flags, final int startId) {
         super.onStartCommand(intent, flags, startId);
@@ -154,11 +158,11 @@ public class AppTrackingService extends LifecycleService {
     }
 
     private void showNotificationIfNecessary() {
-        int oneMinute = 59;
-        int fiveMinutes = 299;
-        int tenMinutes = 599;
-        if (remainingUsageTime == oneMinute || remainingUsageTime == fiveMinutes || remainingUsageTime == tenMinutes) {
-            final Context context = AppTrackingService.this.getApplicationContext();
+
+        if (remainingUsageTime == ONE_MINUTE - NOTIFICATION_TIME_OFFSET
+                || remainingUsageTime == FIVE_MINUTES - NOTIFICATION_TIME_OFFSET
+                || remainingUsageTime == TEN_MINUTES - NOTIFICATION_TIME_OFFSET) {
+            final Context context = getApplicationContext();
             final TimeUpNotificationBuilder builder = new TimeUpNotificationBuilder(context, remainingUsageTime + 1);
             final Notification notification = builder.build();
             final int notificationId = builder.getId();
@@ -171,9 +175,9 @@ public class AppTrackingService extends LifecycleService {
         if (remainingUsageTime == 0 && mSharedPreferencesHandler.getAppsBlacklist().contains(appName)) {
             commitStatus = false;
 
-            final Intent blockIntent = new Intent(AppTrackingService.this, MainActivity.class);
+            final Intent blockIntent = new Intent(getApplicationContext(), MainActivity.class);
             blockIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            AppTrackingService.this.startActivity(blockIntent);
+            startActivity(blockIntent);
         } else {
             commitStatus = true;
         }
@@ -186,13 +190,13 @@ public class AppTrackingService extends LifecycleService {
                 && timeLeft <= TimeCredits.CREDIT_LEARNING.getBlockedMinutes()
                 && mSharedPreferencesHandler.getAppsBlacklist().contains(appName)) {
             commitStatus = false;
-            final Intent blockIntent = new Intent(AppTrackingService.this, MainActivity.class);
+            final Intent blockIntent = new Intent(getApplicationContext(), MainActivity.class);
             blockIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            AppTrackingService.this.startActivity(blockIntent);
+            startActivity(blockIntent);
         } else {
             commitStatus = true;
         }
-    } 
+    }
 
 
 }
