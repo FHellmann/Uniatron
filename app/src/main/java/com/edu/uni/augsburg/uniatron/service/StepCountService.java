0e7e294f01d4e438a6a59339cecb3f6a681990cd
Leon <l.woehrl@gmail.com>
2018-07-01 02:04:02 +0200

    use correct instance of sensormanager

--- app/src/main/java/com/edu/uni/augsburg/uniatron/service/StepCountService.java
+++ app/src/main/java/com/edu/uni/augsburg/uniatron/service/StepCountService.java
@@ -38,7 +38,6 @@ public class StepCountService extends Service implements SensorEventListener {
 
     @Override
     public int onStartCommand(final Intent intent, final int flags, final int startId) {
-        commitSteps(100);
         // grab step detector and register the listener
         final SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -52,7 +51,7 @@ public class StepCountService extends Service implements SensorEventListener {
             final Sensor accelerometerSensor = sensorManager
                     .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sensorManager.registerListener(this,
-                    accelerometerSensor, sensorManager.SENSOR_DELAY_FASTEST);
+                    accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
         } else {
             sensorManager.registerListener(this, stepDetectorSensor,
                     SensorManager.SENSOR_DELAY_FASTEST);
