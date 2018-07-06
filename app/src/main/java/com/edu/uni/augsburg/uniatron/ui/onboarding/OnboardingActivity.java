1f1021656f961c062d7d1ca287a636e0a4823cdf
leonpoint <l.woehrl@gmail.com>
2018-07-07 01:09:07 +0200

    3 pages

--- app/src/main/java/com/edu/uni/augsburg/uniatron/ui/onboarding/OnboardingActivity.java
+++ app/src/main/java/com/edu/uni/augsburg/uniatron/ui/onboarding/OnboardingActivity.java
@@ -1,21 +1,23 @@
 package com.edu.uni.augsburg.uniatron.ui.onboarding;
 
 import android.Manifest;
-import android.graphics.Color;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
+import android.provider.Settings;
+import android.view.View;
 
 import com.edu.uni.augsburg.uniatron.R;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 import com.heinrichreimersoftware.materialintro.slide.Slide;
+import com.rvalerio.fgchecker.Utils;
 
 public class OnboardingActivity extends IntroActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
-        //setFullscreen(true);
+    public void onCreate(Bundle savedInstanceState) {
+        setFullscreen(true);
         super.onCreate(savedInstanceState);
 
         final Slide permissionsSlide;
@@ -31,14 +33,36 @@ public class OnboardingActivity extends IntroActivity {
         addSlide(permissionsSlide);
 
         addSlide(new SimpleSlide.Builder()
+                .title("Uniatron3")
+                .description("You should whitelist our App to make sure your Steps and App are properly tracked")
+                .image(R.drawable.ic_access_time_black_24dp)
+                .background(R.color.onboardingBackground1)
+                .backgroundDark(R.color.onboardingBackground1Dark)
+                .scrollable(true)
+                .buttonCtaLabel("Grant")
+                .buttonCtaClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+                                && !Utils.hasUsageStatsPermission(getApplicationContext())) {
+                            startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+                        }
+
+                    }
+                })
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
                 .title("Uniatron2")
                 .description("You should whitelist our App to make sure your Steps and App are properly tracked")
                 .image(R.drawable.ic_access_time_black_24dp)
                 .background(R.color.onboardingBackground1)
                 .backgroundDark(R.color.onboardingBackground1Dark)
-                .scrollable(false)
+                .scrollable(true)
                 .permission(Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
                 .build());
+
 
     }
 }
