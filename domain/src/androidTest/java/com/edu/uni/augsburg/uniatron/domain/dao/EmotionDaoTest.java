862e718f4abc13e830c8bf1f1ab2459670520c4c
FHellmann <fhellmann93@googlemail.com>
2018-07-26 21:28:57 +0200

    Delete test methods for deleted methods

--- domain/src/androidTest/java/com/edu/uni/augsburg/uniatron/domain/dao/EmotionDaoTest.java
+++ domain/src/androidTest/java/com/edu/uni/augsburg/uniatron/domain/dao/EmotionDaoTest.java
@@ -1,7 +1,6 @@
 package com.edu.uni.augsburg.uniatron.domain.dao;
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule;
-import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -9,7 +8,6 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.edu.uni.augsburg.uniatron.domain.AppDatabase;
 import com.edu.uni.augsburg.uniatron.domain.model.EmotionEntity;
-import com.edu.uni.augsburg.uniatron.domain.util.DateConverter;
 import com.edu.uni.augsburg.uniatron.model.Emotions;
 
 import org.junit.After;
@@ -20,9 +18,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import java.util.Date;
-import java.util.List;
 
-import static com.edu.uni.augsburg.uniatron.domain.util.TestUtils.getLiveDataValue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -57,49 +53,5 @@ public class EmotionDaoTest {
         mDao.add(emotionEntity);
 
         assertThat(emotionEntity.getId(), is(notNullValue()));
-    }
-
-    @Test
-    public void getAll() throws InterruptedException {
-        final EmotionEntity emotionEntity = new EmotionEntity();
-        emotionEntity.setValue(Emotions.NEUTRAL);
-        emotionEntity.setTimestamp(new Date());
-        mDao.add(emotionEntity);
-
-        final EmotionEntity emotionEntity1 = new EmotionEntity();
-        emotionEntity1.setValue(Emotions.HAPPINESS);
-        emotionEntity1.setTimestamp(new Date());
-        mDao.add(emotionEntity1);
-
-        final Date date = new Date();
-        final LiveData<List<EmotionEntity>> liveData = mDao
-                .getAll(DateConverter.DATE_MIN_TIME.convert(date), DateConverter.DATE_MAX_TIME.convert(date));
-
-        final List<EmotionEntity> liveDataValue = getLiveDataValue(liveData);
-        assertThat(liveDataValue, is(notNullValue()));
-        assertThat(liveDataValue.size(), is(2));
-    }
-
-    @Test
-    public void getAverageEmotion() throws InterruptedException {
-        final EmotionEntity emotionEntity = new EmotionEntity();
-        emotionEntity.setValue(Emotions.NEUTRAL);
-        emotionEntity.setTimestamp(new Date());
-        mDao.add(emotionEntity);
-
-        final EmotionEntity emotionEntity1 = new EmotionEntity();
-        emotionEntity1.setValue(Emotions.HAPPINESS);
-        emotionEntity1.setTimestamp(new Date());
-        mDao.add(emotionEntity1);
-
-        final Date date = new Date();
-        final LiveData<Double> liveData = mDao
-                .getAverageEmotion(DateConverter.DATE_MIN_TIME.convert(date), DateConverter.DATE_MAX_TIME.convert(date));
-
-        final double expected = (Emotions.NEUTRAL.ordinal() + Emotions.HAPPINESS.ordinal()) / 2.0;
-
-        final Double liveDataValue = getLiveDataValue(liveData);
-        assertThat(liveDataValue, is(notNullValue()));
-        assertThat(liveDataValue, is(expected));
     }
 }
